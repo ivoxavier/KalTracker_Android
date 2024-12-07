27b8ae7492e3853b397765affcb2252a2804f29e
ixsvf <ivofernandes12@gmail.com>
2024-12-07 23:52:51 +0000

    dev_07122024

--- app/src/main/java/com/ivoxavier/kaltracker/view/MenuActivity.kt
+++ app/src/main/java/com/ivoxavier/kaltracker/view/MenuActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
@@ -29,7 +43,35 @@ class MenuActivity:ComponentActivity() {
                 .fillMaxSize()
                 .padding(16.dp)){
 
-                //TODO: put here the icon of the app inside a card with radius
+                item{
+                    Row(modifier = Modifier
+                        .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Card(modifier = Modifier
+                            .padding(16.dp)
+                            .width(90.dp)
+                            .height(90.dp),
+                            elevation = CardDefaults.cardElevation(
+                                defaultElevation = 10.dp
+                            )
+                        ){
+                            Image(modifier = Modifier
+                                .fillMaxSize(),
+                                    painter = painterResource(id = R.drawable.logo_kaltracker),
+                                contentDescription = null)
+                        }
+                    }
+                    Row(modifier = Modifier
+                        .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center) {
+                        Text(text = "Version: 1.10.0")
+                    }
+                }
+
+                item{
+                Spacer(modifier = Modifier.height(16.dp))
+                }
 
                 item {
                     ListItemHeader("User")
@@ -46,6 +88,10 @@ class MenuActivity:ComponentActivity() {
                     ListItemDivider()
                 }
 
+                item{
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+
                 item {
                     ListItemHeader("Settings")
                     ListItem(text = "Settings", icon = ImageVector.vectorResource(id = R.drawable.ic_settings_24)) {
@@ -56,6 +102,10 @@ class MenuActivity:ComponentActivity() {
                         // TODO: Handle click
                     }
                     ListItemDivider()
+                }
+                
+                item{
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
 
                 item {
@@ -74,3 +124,6 @@ class MenuActivity:ComponentActivity() {
         }
     }
 }
+
+
+
