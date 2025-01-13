fe10a4cda3349c02f72631fa5fcbc1d871bc07d9
ixsvf <ivofernandes12@gmail.com>
2025-01-13 18:53:39 +0000

    dev_13012024

--- app/src/main/java/com/ivoxavier/kaltracker/view/IngestionConfigActivity.kt
+++ app/src/main/java/com/ivoxavier/kaltracker/view/IngestionConfigActivity.kt
@@ -3,6 +3,10 @@ package com.ivoxavier.kaltracker.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -13,11 +17,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import com.ivoxavier.kaltracker.R
 import com.ivoxavier.kaltracker.service.repository.model.Product
 import com.ivoxavier.kaltracker.service.repository.model.UserFoodsListModel
+import com.ivoxavier.kaltracker.view.components.ListItem
 import com.ivoxavier.kaltracker.view.components.ListItemDivider
 import com.ivoxavier.kaltracker.view.components.ListItemHeader
 import com.ivoxavier.kaltracker.view.components.ListItemVerticalSpacer
@@ -32,15 +41,15 @@ class IngestionConfigActivity : ComponentActivity() {
 
         viewModel = ViewModelProvider(this)[IngestionConfigViewModel::class.java]
 
-        // Recebe os dados do produto da Intent
+
         val product = intent.getSerializableExtra("product") as? Product
         viewModel.setProduct(product)
 
-        // Recebe a lista de produtos da Intent lista
+
         val products = intent.getSerializableExtra("products") as? ArrayList<UserFoodsListModel>
         viewModel.setUserFoodsList(products)
 
-        val productIndex = intent.getIntExtra("productIndex", -1) // -1 como valor padrão se não for encontrado
+        val productIndex = intent.getIntExtra("productIndex", -1)
         viewModel.setProductIndex(productIndex)
 
         val mealCategory = intent.getIntExtra("mealCategory", -1)
@@ -71,17 +80,64 @@ fun IngestionConfig(viewModel: IngestionConfigViewModel){
             item {
                 ListItemHeader(title = stringResource(id = R.string.ingestion_config_header_title))
                 ListItemVerticalSpacer()
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically)
+                    {
+                        if(productScanned != null){
+                            productScanned?.productName?.let {
+                                Text(modifier = Modifier.padding(start = 16.dp),
+                                    fontSize = 18.sp,
+                                    text = it
+                                )
+                            }
+                            productScanned?.nutriments?.energyValue?.let {
+                                Text(stringResource(id = R.string.global_string_calories) + ": $it")
+                            }
+                        }
+                        else{
+                            userFoodsListIndex?.let {
+                                userFoodsList?.get(it)?.name?.let {
+                                    Text(modifier = Modifier.padding(start = 16.dp),
+                                        fontSize = 18.sp,
+                                        text = it
+                                    )
+                                }
+                                }
+                            Text("Calorias: ${userFoodsListIndex?.let { userFoodsList?.get(it)?.cal }}")
+                        }
+                }
+
                 if(productScanned != null){
-                    Text("Nome: ${productScanned?.productName}")
-                    Text("Calorias: ${productScanned?.nutriments?.energyValue}")
+                    productScanned?.nutriments?.carbohydrates100g?.let{
+                        ListItem(text = stringResource(id = R.string.global_string_carbo)
+                                + ": " + it ,icon = Icons.Filled.Check ){}
+                    }
+                    productScanned?.nutriments?.proteins100g?.let{
+                        ListItem(text = stringResource(id = R.string.global_string_carbo)
+                                + ": " + it ,icon = Icons.Filled.Check ){}
+                    }
+                    productScanned?.nutriments?.fat100g?.let{
+                        ListItem(text = stringResource(id = R.string.global_string_carbo)
+                                + ": " + it ,icon = Icons.Filled.Check ){}
+                    }
+                } else{
+                    userFoodsListIndex?.let {
+                        userFoodsList?.get(it)?.carbo?.let{
+                            ListItem(text = stringResource(id = R.string.global_string_carbo)
+                                    + ": " + it ,icon = Icons.Filled.Check ){}
+                        }
+                        userFoodsList?.get(it)?.protein?.let{
+                            ListItem(text = stringResource(id = R.string.global_string_protein)
+                                    + ": " + it ,icon = Icons.Filled.Check ){}
+                        }
+                        userFoodsList?.get(it)?.fat?.let{
+                            ListItem(text = stringResource(id = R.string.global_string_fat)
+                                    + ": " + it ,icon = Icons.Filled.Check ){}
+                        }
+                    }
                 }
-                else{
-                    Text("Nome: ${userFoodsListIndex?.let { userFoodsList?.get(it)?.name }}")
-                    Text("Calorias: ${userFoodsListIndex?.let { userFoodsList?.get(it)?.cal }}")
-                }
-                //Text("Carboidrato: 123")
-                //Text("Proteina: 123")
-                //Text("Gordura: 123")
                 ListItemVerticalSpacer()
                 ListItemDivider()
                 ListItemHeader("Tamanho da Porção")
